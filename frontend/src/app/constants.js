ebd3e08e9dcb1420647c91c900cf694e9d5643eb
Aditya-Patil-12 <aditypatil71@gmail.com>
2025-08-01 00:57:36 +0530

    Addded api/v1/ in Backend Deployed URL for frontend

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,3 +1,3 @@
 const productListLimit = 9;
-const baseServerURL = "https://ecommerce-store-tmqu.onrender.com/";
+const baseServerURL = "https://ecommerce-store-tmqu.onrender.com/api/v1/";
 export {productListLimit,baseServerURL};
