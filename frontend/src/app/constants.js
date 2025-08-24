df65a078c72af0ce2d6078081df5307ed9c41c30
Aditya-Patil-12 <aditypatil71@gmail.com>
2025-08-24 18:13:31 +0530

    UnCommented Deployed Settings for Deployment

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,4 +1,4 @@
 const productListLimit = 9;
-// const baseServerURL = "https://ecommerce-store-tmqu.onrender.com/api/v1/";
-const baseServerURL = "http://localhost:5000/api/v1/";
+const baseServerURL = "https://ecommerce-store-tmqu.onrender.com/api/v1/";
+// const baseServerURL = "http://localhost:5000/api/v1/";
 export {productListLimit,baseServerURL};
